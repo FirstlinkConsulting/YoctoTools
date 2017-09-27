90d43bb7f667f3c05d1fc20d16a41cd34f7cfa66
Jean-Pierre Sainfeld <firstlink@comcast.net>
2017-09-26 18:12:44 -0700

    Per variwiki instruction add a shell script for packages
    sudo apt-get install gawk wget git-core diffstat unzip texinfo gcc-multilib build-essential chrpath socat libsdl1.2-dev
    sudo apt-get install autoconf libtool libglib2.0-dev libarchive-dev
    sudo apt-get install python-git xterm sed cvs subversion coreutils texi2html
    sudo apt-get install docbook-utils python-pysqlite2 help2man make gcc g++ desktop-file-utils libgl1-mesa-dev
    sudo apt-get install libglu1-mesa-dev mercurial automake groff curl lzop asciidoc u-boot-tools dos2unix mtd-utils
    sudo apt-get install pv

---
+++ installPackages.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+sudo apt-get install -y gawk wget git-core diffstat unzip texinfo gcc-multilib build-essential chrpath socat libsdl1.2-dev
+sudo apt-get install -y autoconf libtool libglib2.0-dev libarchive-dev
+sudo apt-get install -y python-git xterm sed cvs subversion coreutils texi2html
+sudo apt-get install -y docbook-utils python-pysqlite2 help2man make gcc g++ desktop-file-utils libgl1-mesa-dev
+sudo apt-get install -y libglu1-mesa-dev mercurial automake groff curl lzop asciidoc u-boot-tools dos2unix mtd-utils
+sudo apt-get install -y pv
+
